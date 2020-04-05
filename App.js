8b5a6c09f989e8aab5a53ed4c79ff73963c61bfd
Pedro Luiz Aroma FIlho <pedroaroma@pop-os.localdomain>
2020-04-05 00:08:57 -0300

    (Aula 43) Adicionado Logica adicionar digitos e limpar memoria

--- App.js
+++ App.js
@@ -4,18 +4,39 @@ import {Platform, StyleSheet, Text, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
+const initalState = {
+	displayValue: '0',
+	clearDisplay: false,
+	opration: null,
+	values: [0, 0],
+	current: 0,
+}
+
 export default class App extends Component {
 
-	state = {
-		displayValue: '0'
-	  }
+	state = {...initalState}
 	  
 	  addDigit = n => {
-		  this.setState({displayValue: n})
+		  if (n == '.' && this.state.displayValue.includes('.')){
+			  return
+		  }
+		  const clearDisplay = this.state.displayValue === '0'
+			  || this.state.clearDisplay
+		  const currentValue = clearDisplay ? '' : this.state.displayValue
+		  const displayValue = currentValue + n
+
+		  this.setState({ displayValue, clearDisplay: false })
+
+		  if (n !== '.'){
+			  const newValue = parseFloat(displayValue)
+			  const values = [...this.state.values]
+			  values[this.state.current] = newValue
+			  this.setState({ values })
+		  }
 	  }
 
 	  clearMemory = () => {
-		  this.setState({displayValue: '0'})
+		  this.setState({...initalState})
 	  }
 
 	  setOperation = operation => {
